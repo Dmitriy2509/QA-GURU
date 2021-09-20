df9d6748ef7918ec662766725d7c4bbffa3a5744
Dmitriy S <sakovetsdmytryi@gmail.com>
2021-09-20 20:24:48 +0300

    updated tests after review

--- src/test/java/com/lection1/senenide/HomeworkTest.java
+++ src/test/java/com/lection1/senenide/HomeworkTest.java
@@ -1,6 +1,5 @@
 package com.lection1.senenide;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterAll;
@@ -9,27 +8,16 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomeworkTest {
 
-    public static final String BASE_URL_GITHUB_SELENIDE = "https://github.com/selenide/selenide";
-    public static final String BASE_URL_DRAG_AND_DROP_TEST = "https://the-internet.herokuapp.com/drag_and_drop";
-
-    private By wikiTab = By.cssSelector("span[data-content='Wiki']");
-    private By softAssertionLink = By.xpath("//div[@id='wiki-body']//a[contains(text(), 'Soft assertions')]");
-    private By softAssertinPage = By.xpath("//div[@id='wiki-body']");
-    SelenideElement figureA = $(By.xpath("//*[@id='column-a']"));
-    SelenideElement figureB = $(By.xpath("//*[@id='column-b']"));
-
-    private String junit = "JUnit5";
-    private String exampleCodeFirst = "$(\"#first\").should(visible).click();";
-    private String exampleCodeSecond = "$(\"#second\").should(visible).click();";
-    private String locationA = "A";
-    private String locationB = "B";
+    //locators for 'dragAndDropActionTest' and 'dragAndDropTest'
+    private SelenideElement figureA = $(By.xpath("//*[@id='column-a']"));
+    private SelenideElement figureB = $(By.xpath("//*[@id='column-b']"));
 
     @BeforeAll
     static void setup() {
@@ -43,28 +31,31 @@ public class HomeworkTest {
 
     @Test
     public void githubSelenideTest() {
-        open(BASE_URL_GITHUB_SELENIDE);
-        $(wikiTab).shouldBe(Condition.visible, Duration.ofSeconds(5)).click();
-        $(softAssertionLink).shouldBe(Condition.visible, Duration.ofSeconds(5)).click();
-        $(softAssertinPage).shouldHave(text(junit), text(exampleCodeFirst), text(exampleCodeSecond));
+        open("https://github.com/selenide/selenide");
+        $x("//span[@data-content='Wiki']").click();
+        $x("//*[@id='wiki-pages-filter']").click();
+        $x("//*[@id='wiki-pages-filter']").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $x("//*[contains(text(),'Using JUnit5 extend test class:')]").shouldBe(visible);
     }
 
     @Disabled
     @Test()
     public void dragAndDropActionTest() {
-        open(BASE_URL_DRAG_AND_DROP_TEST);
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         actions().dragAndDrop(figureA, figureB).release().build().perform();
     }
 
     @Test
     public void dragAndDropTest() {
-        open(BASE_URL_DRAG_AND_DROP_TEST);
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         $(figureA).dragAndDropTo(figureB);
-        $(figureA).shouldHave(text(locationB));
-        $(figureB).shouldHave(text(locationA));
+        $(figureA).shouldHave(text("B"));
+        $(figureB).shouldHave(text("A"));
         $(figureA).dragAndDropTo(figureB);
-        $(figureA).shouldHave(text(locationA));
-        $(figureB).shouldHave(text(locationB));
+        $(figureA).shouldHave(text("A"));
+        $(figureB).shouldHave(text("B"));
     }
 
 }
